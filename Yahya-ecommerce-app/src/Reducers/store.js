df292638ab1183195ed5ca7f6e7a37a9cb0be498
E-YAN <48629008+E-YAN@users.noreply.github.com>
2022-08-08 16:08:02 +0300

    Delete Yahya-ecommerce-app directory

--- Yahya-ecommerce-app/src/Reducers/store.js
+++
@@ -1,8 +0,0 @@
-import cartReducer from "./Reducers/cartReducer";
-import { createStore } from "redux";
-
-// createStore creates a Redux store that holds the complete app state.
-// There should only be a single store in your app.
-const store = createStore(cartReducer);
-
-export default store;
